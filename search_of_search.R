c00bda4470777f4ab50dc11ba8b38dd2f5868950
Miguel Tokumoto <m.tokumoto@gmail.com>
2020-08-10 15:09:42 -0500

    second

---
+++ search_of_search.R
@@ -0,0 +1,26 @@
+#### Share of Search ####
+library(gtrendsR); library(tidyverse); library(ggplot2); library(scales)
+library(ggthemes); library(magrittr); library(zoo)
+
+search_data <- gtrends(keyword = c('metro', 'wong', 'plaza vea', 'tottus', 
+                                   'makro'),
+        geo = 'PE', time = 'all', gprop = 'web')
+search_data <- search_data$interest_over_time
+search_data$hits <- as.numeric(search_data$hits)
+search_data$hits[is.na(search_data$hits)] <- 0
+
+roll_data <- search_data %>% 
+  group_by(date) %>% 
+  arrange(date) %>% 
+  mutate(rollbase = sum(hits)) %>%  
+  ungroup() %>% 
+  group_by(keyword) %>% 
+  mutate(rollmean = rollmean(x = rollbase, k = 12, na.pad = TRUE, 
+                             align = 'right')) %>% 
+  mutate(rollkey = rollmean(x = hits, k = 12, na.pad = TRUE,
+                            align = 'right')) %>% 
+  ungroup() %>% 
+  mutate(SOS = hits/rollmean)
+
+ggplot(data = roll_data, aes(x = date, y = SOS, fill = keyword)) +
+  geom_area()
